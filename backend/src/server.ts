aae969f66a5e551312459be551ce0e2a6dd573d9
Luiz.Uliana <luizeduardr@gmail.com>
2020-06-02 18:15:25 -0300

    Criação de controllers, migrations, e rotas da aplicação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,15 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
+app.use(cors())
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json([
-        "Diego",
-        "Mike",
-        "Luiz",
-        "Renan"
-    ])
-})
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
